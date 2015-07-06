5db0addbb7907e45a5911b93ea4e91c62378b9a1
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-07-06 16:41:49 +0800

    Commented out LogEntry record with nippy freezing/thawing.

--- src/onyx/compression/nippy.clj
+++ src/onyx/compression/nippy.clj
@@ -7,3 +7,18 @@
 (defn decompress [x]
   (nippy/thaw x {:v1-compatibility? false}))
 
+; (defrecord LogEntry [fn args])
+
+; (nippy/extend-freeze LogEntry :onyx/le ; A unique (namespaced) type identifier
+;                      [x data-output]
+;                      (nippy/freeze-to-out! data-output (:fn x))
+;                      (nippy/freeze-to-out! data-output (:args x)))
+
+; (nippy/extend-thaw :onyx/le ; Same type id
+;                    [data-input]
+;                    (->LogEntry (nippy/thaw-from-in! data-input)
+;                                (nippy/thaw-from-in! data-input)))
+
+
+ ;(alength (compress (->LogEntry 1 2)))
+; (alength (compress {:fn :a :args :b} ))
